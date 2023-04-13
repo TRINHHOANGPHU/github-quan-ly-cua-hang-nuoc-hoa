18fec73b056aecfbfb1b50c60874cf05d2db4df8
fuvx22 <voxngocphu@gmail.com>
2023-04-13 22:58:32 +0700

    update

--- src/main/java/GUI/InnerContent.java
+++ src/main/java/GUI/InnerContent.java
@@ -4,24 +4,25 @@ import java.awt.Color;
 
 import GUI_ChiTietPhieuNhap.ChiTietPhieuNhap_VIEW;
 import GUI_NhaCungCap.NhaCungCap_VIEW;
+import GUI_PhieuNhap.PhieuNhap_VIEW;
 
 public class InnerContent {
 
 	public InnerContent() {
 		if (MenuLeft_CONTROLLER.flag == 1) {
-			Content_VIEW.jPanel_Content.setBackground(Color.BLACK);
+			
 		} else if (MenuLeft_CONTROLLER.flag == 2) {
-			Content_VIEW.jPanel_Content.setBackground(Color.GREEN);
+			
 		} else if (MenuLeft_CONTROLLER.flag == 3) {
-			Content_VIEW.jPanel_Content.setBackground(Color.BLUE);
+			
 		} else if (MenuLeft_CONTROLLER.flag == 4) {
-			Content_VIEW.jPanel_Content.setBackground(Color.PINK);
+			
 		} else if (MenuLeft_CONTROLLER.flag == 5) {
 			
 		} else if (MenuLeft_CONTROLLER.flag == 6) {
 			new ChiTietPhieuNhap_VIEW();
 		} else if (MenuLeft_CONTROLLER.flag == 7) {
-			
+			new PhieuNhap_VIEW();
 		} else if (MenuLeft_CONTROLLER.flag == 8) {
 			new NhaCungCap_VIEW();
 		} else if (MenuLeft_CONTROLLER.flag == 9) {
